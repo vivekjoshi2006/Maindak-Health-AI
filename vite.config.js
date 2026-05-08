6a58bd03c9aa0e6909ba6284b66458f5cd1d420f
Vivek Joshi <vivekjoshi41996@gmail.com>
2026-05-08 08:04:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600, 
+  },
+  server: {
+    fs: {
+      strict: false
+    }
+  }
 })
